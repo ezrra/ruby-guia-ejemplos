acfa032b8328a911965e792206713a3c137c53eb
israel martinez <israel.martinez.vargas@gmail.com>
2015-09-18 15:09:42 -0700

    array y hashes ejemplos

---
+++ array_hashes.rb
@@ -0,0 +1,19 @@
+numbers = [0, 1, 2]
+
+puts numbers.join(':')
+
+###
+
+h = {1=>2, "2"=>"cuatro"}
+
+puts h["2"]
+
+###
+
+h[5]
+
+h[5] = "Cinco"
+
+h[1] = nil
+
+puts h
---
+++ expresiones_regulares.rb
@@ -0,0 +1,20 @@
+# terminal ANSI
+
+st = "\033[7m"
+en = "\033[m"
+
+while TRUE
+	print "str> "
+	STDOUT.flush
+	str = gets
+	break if not str
+	str.chop!
+	print "pat> "
+	STDOUT.flush
+	re = gets
+	break if not re
+	re.chop!
+	str.gsub! re, "#{st}\\&#{en}"
+	print str, "\n"
+end
+print "\n"
