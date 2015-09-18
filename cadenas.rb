900681480a54e9738b07455254831becba879965
israel martinez <israel.martinez.vargas@gmail.com>
2015-09-18 14:37:11 -0700

    Ejemplo de cadenas

---
+++ cadenas.rb
@@ -0,0 +1,38 @@
+###
+
+str = "foo" * 4
+puts str
+
+###
+
+word = "fo" + "0"
+
+###
+puts word[0, 2]
+
+###
+if "FOOT" == "foot"
+	puts 'true'
+else
+	puts 'false'
+end
+
+###
+
+words = ['foo', 'baz', 'qux']
+
+secret = words[rand(3)]
+
+print "Adivinar..."
+
+while guess = STDIN.gets
+	guess.chop!
+	if guess == secret
+		print "ADIVINASTE"
+		break
+	else
+		print "Nop..."
+	end
+	print "Adivinar"
+end
+print "Era la palabra: ", secret, ".\n"
